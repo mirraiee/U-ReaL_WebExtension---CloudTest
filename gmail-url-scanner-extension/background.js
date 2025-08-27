9f069d4f7445cca36c006c332356df950fdf403a
Cardpatr <patrickcardenio@yahoo.com>
2025-08-27 17:20:08 +0800

    add privacy policy
    add

--- gmail-url-scanner-extension/background.js
+++ gmail-url-scanner-extension/background.js
@@ -1,7 +1,7 @@
 // Background Service Worker for Gmail URL Scanner
 
 // Backend API configuration
-const BACKEND_URL = 'http://3.0.57.177:5000'; // Use your EC2 instance's public IP
+const BACKEND_URL = 'http://54.169.37.142:5000'; // Use your EC2 instance's public IP
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'checkURL') {
